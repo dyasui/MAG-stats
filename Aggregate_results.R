6ae3395fe85531799911458ab7345ac6df9a5b8d
Dante <dyasui@uoregon.edu>
2024-07-25 21:55:13 -0700

    updated results

--- Aggregate_results.R
+++ Aggregate_results.R
@@ -1,19 +1,10 @@
 # aggregating results from major national and international competitions
 library(tidyverse)
 
-# Selected Olympic teams:
-olympic_teams <- read_csv("./olympic_teams.csv")
-olympians <- olympic_teams %>% 
-  select(NAME) %>% 
-  pull()
-
-bgc_df   <- read_csv("./GreatBritain2024/BritChamps_results.csv") %>% 
-  mutate(TEAM = "GBR")
-
-nc1_df <- read_csv("./OlympicTrials2024/natnls-d1.csv")
-nc2_df <- read_csv("./OlympicTrials2024/natnls-d2.csv")
-ot1_df <- read_csv("./OlympicTrials2024/trials-day1.csv")
-ot2_df <- read_csv("./OlympicTrials2024/trials-day2.csv")
+nc1_df <- read_csv("./USA2024/natnls-d1.csv")
+nc2_df <- read_csv("./USA2024/natnls-d2.csv")
+ot1_df <- read_csv("./USA2024/trials-day1.csv")
+ot2_df <- read_csv("./USA2024/trials-day2.csv")
 usa_df <- bind_rows(list(nc1_df, nc2_df, ot1_df, ot2_df)) %>% 
   mutate(TEAM = "USA")
 
@@ -27,111 +18,7 @@ results_files <- str_c("./results/", list.files("./results"))
 results_other <- read_csv(results_files)
 
 # merge all results into one dataframe
-results_df <- bind_rows(list(bgc_df, usa_df, jpn_df, results_other)) %>% 
-  filter(NAME %in% olympians)
+results_df <- bind_rows(list( usa_df, jpn_df, results_other)) %>% 
+  select(MEET, ROUND, NAME, TEAM, FX:AA, FX_D:HB_D)
 
-library(ggthemes)
-eventsdist <- results_df %>% 
-  filter(NAME %in% olympians) %>% 
-  select(c(NAME, TEAM, FX, PH, SR, VT, PB, HB)) %>%
-  pivot_longer(
-    cols = c(FX, PH, SR, VT, PB, HB),
-    names_to = "Event",
-    values_to = "Score",
-  ) %>% filter(Score > 1) %>%
-  ggplot(aes(x=Score)) +
-  geom_density() +
-  xlim(11, 16) +
-  facet_wrap(~Event, ncol = 2) +
-  theme_pander()
-
-event_averages <- results_df %>% select(!AA) %>%
-  filter(NAME %in% olympians) %>% 
-  pivot_longer(
-    cols = c(FX, PH, SR, VT, PB, HB),
-    names_to = "Event",
-    values_to = "Score",
-  ) %>% 
-  group_by(Event) %>% 
-  summarise(mean = mean(Score, na.rm = T),
-            sd   = sd(Score, na.rm = T))
-
-eventsdist + 
-  geom_vline(data = event_averages, aes(xintercept = mean)) +
-  geom_vline(data = event_averages, aes(xintercept = mean + sd), linetype = "dashed") +
-  geom_vline(data = event_averages, aes(xintercept = mean - sd), linetype = "dashed")
-
-indv.averages_df <- results_df %>% 
-  group_by(NAME, TEAM) %>% 
-  summarise_at(vars(FX:AA), mean, na.rm=TRUE) %>% 
-  arrange(desc(AA)) 
-
-indv.averages_df <- indv.averages_df %>% 
-  filter(NAME %in% olympians)
-
-# function to pick top three scores on each event from a team
-team_top_scores <- function(names, scores = indv.averages_df, perevent = 3) {
-  scores %>% 
-    filter(NAME %in% names) %>%
-    select(NAME, FX, PH, SR, VT, PB, HB) %>% 
-    pivot_longer(
-      -NAME,
-      names_to = "Event",
-      values_to = "Score",
-    ) %>% 
-    # take top 3 on each event dropping extra tied scores
-    group_by(Event) %>% 
-    slice_max(n = perevent, order_by = Score, with_ties = FALSE) %>% 
-    mutate(
-      teamscore = sum(Score),
-      # names     = c(unique(NAME))
-      )  %>%
-    group_by(Event) %>% 
-    summarise(
-      Score   = first(teamscore),
-      OnEvent = list((NAME))
-      ) %>% ungroup()
-}
-
-# function to take output of select_top_scores and calculate total
-team_total <- function(top_scores) {
-  top_scores %>% 
-    summarise(x=sum(Score)) %>% 
-    pull()
-}
-
-
-results_df %>% 
-  group_by(TEAM) %>% 
-  summarise(score = team_total(team_top_scores(unlist(list(unique(NAME)))))) %>% 
-  arrange(desc(score))
-
-team_jpn <- results_df %>% 
-  filter(TEAM == "JPN") %>% 
-  pull(NAME) %>% 
-  unique() %>% 
-  team_top_scores() 
-
-team_usa <- results_df %>% 
-  filter(TEAM == "USA") %>% 
-  pull(NAME) %>% 
-  unique() %>% 
-  team_top_scores() 
-  
-team_gbr <- indv.averages_df %>% 
-  filter(TEAM == "GBR") %>% 
-  pull(NAME) %>% 
-  unique() %>% 
-  team_top_scores()
-
-team_ukr <- indv.averages_df %>% 
-  filter(TEAM == "UKR") %>% 
-  pull(NAME) %>% 
-  unique() %>% 
-  team_top_scores()
-
-
-team_jpn %>% kableExtra::kable()
-team_usa %>% kableExtra::kable()
-team_gbr %>% kableExtra::kable()
-team_ukr %>% kableExtra::kable()
+write_csv(results_df, "./ParisOlympics2024/2024-aggregate-results.csv")
--- webscrape_gymternet.R
+++ webscrape_gymternet.R
@@ -23,8 +23,11 @@ read_gymternet_EV <- function(table, label) {
   table %>% 
     mutate( 
       across( # convert DNS, —— to NA and make numeric for event results
-        where(is.character) & matches(c("D", "E", "Total")),
+        where(is.character) & matches(c("\\bD\\b", "\\bE\\b", "Total")),
         ~as.numeric(na_if(., "DNS")) ),
+      across( # convert DNS, —— to NA and make numeric for event results
+        where(is.character) & matches(c("\\bD\\b", "\\bE\\b", "Total")),
+        ~as.numeric(na_if(., "——")) ),
       EVENT = case_when(
         str_detect(label, "Floor|FX") ~ "FX",
         str_detect(label, "Pommel|PH") ~ "PH",
@@ -35,8 +38,8 @@ read_gymternet_EV <- function(table, label) {
       ),
       ROUND = label
     ) %>% 
-    rename( NAME = Athlete, TEAM = Nation) %>% 
-    select(EVENT, ROUND, NAME, TEAM, D, E, Total)
+    rename( NAME = Athlete) %>% 
+    select(EVENT, ROUND, NAME:E, Total)
 }
 
 # function to combine multiple event and aa finals from an international meet
@@ -54,6 +57,12 @@ read_gymternet_international <- function(url, meet_name) {
   # initialize the loop:
   i = 1; j = 1; k = 1 # index for all tables, aa tables, and ev tables
   table_aa = list(); table_ev = list()
+  aa_results = event_results = tibble(
+    ROUND=character(), NAME=character(), TEAM=character(), FX=numeric(), 
+    PH=numeric(), SR=numeric(), VT=numeric(), PB=numeric(), HB=numeric(), 
+    AA=numeric(), FX_D=numeric(), PH_D=numeric(), SR_D=numeric(), VT_D=numeric(),
+    PB_D=numeric(), HB_D=numeric()
+  )
   table_list <- webpage %>% html_table(header = TRUE)
   # loop over all tables (indexed by their headers):
   for (header in headers) {
@@ -75,11 +84,14 @@ read_gymternet_international <- function(url, meet_name) {
     i = i + 1
   }
   
-  aa_results <- bind_rows(table_aa)
+  if (!is_empty(table_aa)) {
+    aa_results <- bind_rows(table_aa)
+    }
   
-  event_results <- bind_rows(table_ev) %>% 
+  if (!is_empty(table_ev)) { 
+    event_results <- bind_rows(table_ev) %>% 
     pivot_wider(
-      id_cols = c(ROUND, NAME, TEAM),
+      id_cols = c(ROUND, NAME, Nation),
       names_from = EVENT,
       values_from = c(D, E, Total), 
       values_fn = max # two vaults are annoying to keep track of
@@ -88,7 +100,9 @@ read_gymternet_international <- function(url, meet_name) {
       FX = Total_FX, PH = Total_PH, SR = Total_SR, VT = Total_VT, PB = Total_PB, HB = Total_HB,
       FX_D = D_FX, PH_D = D_PH, SR_D = D_SR, VT_D = D_VT, PB_D = D_PB, HB_D = D_HB,
       FX_E = E_FX, PH_E = E_PH, SR_E = E_SR, VT_E = E_VT, PB_E = E_PB, HB_E = E_HB) %>% 
-    mutate(AA = NA)
+    mutate(AA = NA, TEAM = Nation)
+  }
+  
   
   results <- bind_rows(list(aa_results, event_results)) %>% 
     mutate(
@@ -108,47 +122,128 @@ save_gym_results <- function(results, year = "2024") {
   filename <- paste("./results/", meet_name, "-", year, ".csv", sep = "")
   write_csv(results, file = filename)
 }
+  
+# meet_tbl <- tibble(
+#   url = c(
+#     "https://thegymter.net/2024/04/29/2024-european-mens-championships-results/",
+#     "https://thegymter.net/2024/02/18/2024-cairo-world-cup-mens-results/",
+#     "https://thegymter.net/2024/02/27/2024-cottbus-world-cup-mens-results/",
+#     "https://thegymter.net/2024/03/10/2024-baku-world-cup-mens-results/",
+#     "https://thegymter.net/2024/03/19/2024-dtb-pokal-team-challenge-mens-results/",
+#     "https://thegymter.net/2024/03/23/2024-antalya-friendly-results/",
+#     "https://thegymter.net/2024/04/02/2024-antalya-challenge-cup-mens-results/",
+#     "https://thegymter.net/2024/04/08/2024-osijek-challenge-cup-mens-results/",
+#     "https://thegymter.net/2024/04/20/2024-doha-world-cup-mens-results/",
+#     "https://thegymter.net/2024/04/29/2024-pacific-rim-championships-mens-results/",
+#     "https://thegymter.net/2024/05/21/2024-asian-championships-mens-results/",
+#     "https://thegymter.net/2024/05/28/2024-pan-american-championships-mens-results/",
+#     "https://thegymter.net/2024/05/28/2024-varna-challenge-cup-mens-results/",
+#     "https://thegymter.net/2024/06/03/2024-koper-challenge-cup-mens-results/"
+#   ),
+#   meet_name = c(
+#     "euro_champs",
+#     "cairo_cup",
+#     "cottbus_cup",
+#     "baku_cup",
+#     "dtppokal",
+#     "antalya_friendly",
+#     "antalya_cup",
+#     "osijek_cup",
+#     "doha_cup",
+#     "pacrim_champs",
+#     "asian_champs",
+#     "panamer_champs",
+#     "varna_cup",
+#     "koper_cup"
+#   )
+# )
 
-meet_tbl <- tibble(
-  url = c(
-    "https://thegymter.net/2024/04/29/2024-european-mens-championships-results/",
-    "https://thegymter.net/2024/02/18/2024-cairo-world-cup-mens-results/",
-    "https://thegymter.net/2024/02/27/2024-cottbus-world-cup-mens-results/",
-    "https://thegymter.net/2024/03/10/2024-baku-world-cup-mens-results/",
-    "https://thegymter.net/2024/03/19/2024-dtb-pokal-team-challenge-mens-results/",
-    "https://thegymter.net/2024/03/23/2024-antalya-friendly-results/",
-    "https://thegymter.net/2024/04/02/2024-antalya-challenge-cup-mens-results/",
-    "https://thegymter.net/2024/04/08/2024-osijek-challenge-cup-mens-results/",
-    "https://thegymter.net/2024/04/20/2024-doha-world-cup-mens-results/",
-    "https://thegymter.net/2024/04/29/2024-pacific-rim-championships-mens-results/",
-    "https://thegymter.net/2024/05/21/2024-asian-championships-mens-results/",
-    "https://thegymter.net/2024/05/28/2024-pan-american-championships-mens-results/",
-    "https://thegymter.net/2024/05/28/2024-varna-challenge-cup-mens-results/",
-    "https://thegymter.net/2024/06/03/2024-koper-challenge-cup-mens-results/"
-  ),
-  meet_name = c(
-    "euro_champs",
-    "cairo_cup",
-    "cottbus_cup",
-    "baku_cup",
-    "dtppokal",
-    "antalya_friendly",
-    "antalya_cup",
-    "osijek_cup",
-    "doha_cup",
-    "pacrim_champs",
-    "asian_champs",
-    "panamer_champs",
-    "varna_cup",
-    "koper_cup"
+
+read_gymternet_international(
+  url = 
+    "https://thegymter.net/2023/09/30/2022-asian-games-mens-results/",
+  meet_name = 
+    "asian_games"
+) %>% 
+save_gym_results(year = "2023")
+
+### National-level Competitions ###
+###################################
+
+read_gymternet_national <- function(url, meet_name) {
+  webpage <- read_html(url)
+  # save all results tables in a list
+  table_list <- webpage %>% html_table(header = TRUE)
+  
+  # extract event and round info from headers above each table
+  headers <- (webpage %>% html_elements("p") %>% html_text2() )
+  # drop the first element of headers (the description),
+  # and the last two which are just formatting
+  headers <- headers[2:(length(headers)-2)]
+  
+  # initialize the loop:
+  i = 1; j = 1; k = 1 # index for all tables, aa tables, and ev tables
+  table_aa = list(); table_ev = list()
+  aa_results = event_results = tibble(
+    ROUND=character(), NAME=character(), TEAM=character(), FX=numeric(), 
+    PH=numeric(), SR=numeric(), VT=numeric(), PB=numeric(), HB=numeric(), 
+    AA=numeric(), FX_D=numeric(), PH_D=numeric(), SR_D=numeric(), VT_D=numeric(),
+    PB_D=numeric(), HB_D=numeric()
   )
-)
-
-
-for (i in nrow(meet_tbl)) {
-  read_gymternet_international(
-    url = meet_tbl$url[i],
-    meet_name = meet_tbl$meet_name[i]
-  ) %>% 
-    save_gym_results()
+  table_list <- webpage %>% html_table(header = TRUE)
+  # loop over all tables (indexed by their headers):
+  for (header in headers) {
+    if(str_detect(header, "Junior")) { 
+      break # don't include results for juniors
+    } 
+    else if (str_detect(header, "All-Around")) {
+      table_aa[[j]] <- read_gymternet_AA(table_list[[i]], header)
+      j = j + 1
+    } else if (
+      str_detect(header, 
+                 c("Event|Floor|Pommel|Rings|Vault|P-Bars|High Bar|FX|PH|SR|VT|PB|HB")
+      ) ) {
+      table_ev[[k]] <- read_gymternet_EV(table_list[[i]], header) %>% 
+        rename(TEAM = Team)
+      k = k + 1
+    } else {
+      break 
+    }
+    i = i + 1
+  }
+  
+  if (!is_empty(table_aa)) {
+    aa_results <- bind_rows(table_aa)
+    }
+  
+  if (!is_empty(table_ev)) { 
+    event_results <- bind_rows(table_ev) %>% 
+    pivot_wider(
+      id_cols = c(ROUND, NAME, TEAM),
+      names_from = EVENT,
+      values_from = c(D, E, Total), 
+      values_fn = max # two vaults are annoying to keep track of
+    ) %>% 
+    rename(
+      FX = Total_FX, PH = Total_PH, SR = Total_SR, VT = Total_VT, PB = Total_PB, HB = Total_HB,
+      FX_D = D_FX, PH_D = D_PH, SR_D = D_SR, VT_D = D_VT, PB_D = D_PB, HB_D = D_HB,
+      FX_E = E_FX, PH_E = E_PH, SR_E = E_SR, VT_E = E_VT, PB_E = E_PB, HB_E = E_HB) %>% 
+    mutate(AA = NA)
+  }
+  
+  results <- bind_rows(list(aa_results, event_results)) %>% 
+    mutate(
+      MEET = meet_name,
+    ) %>% 
+    select(MEET, ROUND, NAME, TEAM, FX, PH, SR, VT, PB, HB, AA,
+           FX_D, PH_D, SR_D, VT_D, PB_D, HB_D)
+  
+  results
 }
+
+read_gymternet_national(
+  "https://thegymter.net/2024/04/30/2024-chinese-championships-mens-results/",
+  "china_champs"
+  ) %>% 
+  mutate(TEAM = "CHN") %>% 
+  write_csv(file = "./results/china_champs-2024.csv")
